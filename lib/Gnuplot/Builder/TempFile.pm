6e49b3a2f04669c7d7455e1b165c152d5fe1d5b0
Toshio Ito <debug.ito@gmail.com>
2014-02-24 14:59:02 +0900

    TempFile: write POD.

--- lib/Gnuplot/Builder/TempFile.pm
+++ lib/Gnuplot/Builder/TempFile.pm
@@ -11,13 +11,87 @@ __END__
 
 =head1 NAME
 
-Gnuplot::Builder::TempFile - abstract
+Gnuplot::Builder::TempFile - gnuplot wrapper using temporary files
 
 =head1 SYNOPSIS
 
+    use Gnuplot::Builder::Process;
+    
+    @Gnuplot::Builder::Process::COMMAND
+        = qw(gnuplot_builder_tempfile_wrapper gnuplot --persist);
+
 =head1 DESCRIPTION
 
+L<Gnuplot::Builder::TempFile> is an implementation of C<gnuplot_builder_tempfile_wrapper> command
+bundled in this distribution.
+
+C<gnuplot_builder_tempfile_wrapper> wraps a gnuplot process with a temporary file.
+It receives script text from STDIN, stores the text in the temporary file and executes the real gnuplot
+with the temporary file.
+It also tries to clean up the temporary file it created.
+
+C<gnuplot_builder_tempfile_wrapper> is meant to be used with L<Gnuplot::Builder> as a replacement of
+the real gnuplot command. See the L</SYNOPSIS> section for usage.
+
+=head2 Why Do I Need This?
+
+Usually you don't. It is much more efficient to streaming script into the real gnuplot process.
+
+However, in some platforms (such as Windows) gnuplot behaves strangely when you stream script with a pipe.
+I found it especially difficult to keep plot windows open, so I made this.
+
+If you are in trouble with Windows gnuplot, L<Gnuplot::Builder::WgnuplotWrapper> may help.
+
+
+=head1 FUNCTIONS
+
+All functions are exported only by request.
+
+=head2 run(@gnuplot_command)
+
+This function executes the following steps.
+
+=over
+
+=item 1.
+
+Open a temporary file.
+
+=item 2.
+
+Receive data from STDIN and write the data to the temporary file.
+
+=item 3.
+
+After STDIN is closed, execute C<@gnuplot_command> in a separate process
+with the path to the temporary file appended to the arguments.
+
+This is basically the same as
+
+    system(@gnuplot_command, $path_to_temp_file);
+
+except that it won't wait for the process.
+
+If C<@gnuplot_command> is empty, it uses C<< ("gnuplot", "--persist") >> by default.
+
+=item 4.
+
+Execute C<gnuplot_builder_tempfile_remover> bundled in this distribution in a seprate process.
+
+This is basically the same as
+
+    system("gnuplot_builder_tempfile_remover", $path_to_temp_file);
+
+except that it won't wait for the process.
+
+C<gnuplot_builder_tempfile_remover> removes the temporary file after waiting for a while, then exits.
+
+=back
+
+
 =head1 SEE ALSO
+
+L<Gnuplot::Builder>
 
 =head1 REPOSITORY
 
